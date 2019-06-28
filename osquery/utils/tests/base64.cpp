7be9d6d209492b826d344620d6d076da4314380b
ggrieco-tob <gustavo.grieco@trailofbits.com>
2019-06-28 17:03:04 -0300

    first approach to compile with deepstate

--- osquery/utils/tests/base64.cpp
+++ osquery/utils/tests/base64.cpp
@@ -6,21 +6,26 @@
  *  the LICENSE file found in the root directory of this source tree.
  */
 
-#include <gtest/gtest.h>
-
+//#include <gtest/gtest.h>
+#include <deepstate/DeepState.hpp>
 #include <osquery/utils/base64.h>
 
 namespace osquery {
 
-class Base64Tests : public testing::Test {};
+class Base64Tests : public deepstate::Test {
+};
+
 
 TEST_F(Base64Tests, test_base64) {
-  std::string unencoded = "HELLO";
+
+  deepstate::ForAll<std::string>([] (const std::string &unencoded) {
   auto encoded = base64::encode(unencoded);
-  EXPECT_NE(encoded.size(), 0U);
+  //ASSERT_L(encoded.size(), 0U);
 
   auto unencoded2 = base64::decode(encoded);
-  EXPECT_EQ(unencoded, unencoded2);
+  ASSERT_EQ(unencoded, unencoded2);
+  });
 }
 
 } // namespace osquery
+
